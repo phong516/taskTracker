d8e59e1adc199aef050a05623cc955bd7f2199ec
Phong <tiphong113@gmail.com>
2025-09-29 23:00:38 +0700

    update baseline json

--- src/misc/json.cpp
+++ src/misc/json.cpp
@@ -4,3 +4,35 @@ Json::Json(std::string string)
 {
     std::cout << string << std::endl;
 }
+
+
+json::json(const std::string & filePath)
+{
+    std::ifstream file(filePath);
+    std::string line {};
+
+    while (std::getline(file, line))
+    {
+       parsedJson.append(line); 
+    }
+}
+
+jsonValue parse(const std::string & text, std::string::iterator start, std::string::iterator end)
+{
+    std::string substr = text.substr(start - text.begin(), end - start);
+    size_t dotIndex = substr.find('.');
+    if (dotIndex >= (end - start))
+    {
+        return
+        {
+            .i = std::stoi(substr)
+        };
+    }
+    else
+    {
+        return
+        {
+            .f = std::stof(substr)
+        };
+    }
+}
--- src/misc/json.h
+++ src/misc/json.h
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
-class Json
+union jsonValue
 {
-public:
-    Json(std::string string);
+    int i;
+    float f;
+    std::map<std::string, jsonValue> * json;
+};
+struct jsonPair 
+{
+    std::string key {};
+    jsonValue value {};
+};
+
+class json
+{
+    public:
+        json(const std::string jsonStr): parsedJson(jsonStr) {}
+        json(const std::string & filePath);
+    private:
+        std::string parsedJson;
+        jsonValue parse(const std::string & text, std::string::iterator start, std::string::iterator end);
 };
